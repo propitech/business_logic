0f0fd4fcba8fa2e250d6cb10a5ad06d2d321fdb7
Hallelujah <hery@rails-royce.org>
2025-11-12 15:32:41 +0100

    Fix reek too many statement

--- lib/generators/business_logic/install/install_generator.rb
+++ lib/generators/business_logic/install/install_generator.rb
@@ -8,9 +8,19 @@ module BusinessLogic
     include Helper
 
     def install
+      copy_files
+      install_gems
+    end
+
+    private
+
+    def copy_files
       copy_file "application_operation.rb.tt", "#{install_path}/application_operation.rb"
       copy_file "application_contract.rb.tt", "#{install_path}/application_contract.rb"
       copy_file "spec_generators_helper.rb.tt", "#{tests_path}/generators_helper.rb"
+    end
+
+    def install_gems
       gem "ammeter", "~> 1.1", group: :test
       gem "dry-initializer", "~> 3.1"
       gem "dry-operation", "~> 1.0"
